a0605281d17fda2b252e8afc58b70a5c17c5bd4e
Alfredo <alibarra@csumb.edu>
2023-03-21 13:32:43 -0700

    fixed Variable error

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -28,7 +28,7 @@ class AddStudent extends Component{
         this.setState({email:{student_email: event.target.value}});
       }
       handleAdd = () => {
-        this.props.AddStudent(this.state.name,this.state.email);
+        this.props.addStudent(this.state.name,this.state.email);
         this.handleClose();
       }
     render()  {
